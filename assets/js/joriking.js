edb27c6ec25328489eaa85ec0c0eafac428d2687
Choi Seungwon <cso6549@naver.com>
2023-05-25 13:56:38 +0900

    update joriking

--- assets/js/joriking.js
+++ assets/js/joriking.js
@@ -1,16 +1,19 @@
 const proxy = "http://127.0.0.1:8000"
 
 // 입력된 정보를 fomdata형식으로 변환 후 이미지 업로드 함수로 전달하는 함수
-function handleImage(event) {
+async function handleImage(event) {
     event.preventDefault(); // 제출 버튼을 눌렀을 때 새로고침 방지
 
     const image = document.getElementById('chooseFile').files[0];
+    const loader = document.querySelector('#loader')
 
     // FormData를 사용하면 header에 "application/json"을 담지 않아도 됨
     const formData = new FormData();
     formData.append('image', image);
 
-    imageUpload(formData);
+    loader.style.display = 'block'
+    await imageUpload(formData);
+    loader.style.display = 'none'
 }
 
 // 전달받은 값으로 백엔드에 요청하는 이미지 업로드 함수
@@ -43,16 +46,35 @@ async function imageUpload(formData) {
 
 function loadJoriking(data) {
 
-    const image1Src = proxy + data.data.image;
-    const image2Src = proxy + data.data.pred_path;
+    const imageSrc = proxy + data.data.pred_path;
+    const formattedResult = data.result.replace(/\n/g, "<br>");
 
     const jorikingElement = document.getElementById('joriking');
     jorikingElement.innerHTML = `
-    <image src=${image1Src}></image>
-    <image src=${image2Src}></image>
-    <p>${data.result}</p>
+    <image src=${imageSrc}></image>
+    <p>${formattedResult}</p>
     `
+
 }
+
+function loadFile(input) {
+    var file = input.files[0];
+
+    var newImage = document.createElement("img");
+    newImage.setAttribute("class", 'img');
+
+    newImage.src = URL.createObjectURL(file);
+
+    // newImage.style.width = "100%";
+    // newImage.style.height = "100%";
+    // newImage.style.objectFit = "contain";
+
+    var container = document.getElementById('image-show');
+    container.innerHTML = '';
+    container.appendChild(newImage);
+};
+
+
 
 const form = document.getElementById('submitButton');    // submitButton이라는 요소를 찾아 form에 할당
 form.addEventListener('click', handleImage);  // 이벤트 발생 시 handleSignup함수 실행
