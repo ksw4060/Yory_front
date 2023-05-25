32dbd6a00681a1dcc634273c30733c71f4534104
wooseok <william741@naver.com>
2023-05-25 21:12:27 +0900

    게시글 작성 js

---
+++ assets/js/article_create.js
@@ -0,0 +1,51 @@
+const proxy = "https://api.bechol.com"
+
+function handleCreateArticle(event) {
+    event.preventDefault(); // 제출 버튼을 눌렀을 때 새로고침 방지
+
+    const title = document.getElementById('article_title').value;
+    const category = document.getElementById('category').value;
+    const image = document.getElementById('myFile').files[0];
+    const content = $('#summernote').summernote('code');
+
+
+    // FormData를 사용하면 header에 "application/json"을 담지 않아도 됨
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('category', category);
+    formData.append('content', content);
+    formData.append('image', image);
+    console.log(title, category, image, content);
+    console.log(formData.keys());
+
+    createArticle(formData);
+};
+
+async function createArticle(formData) {
+    try {
+        // 로컬스토리지에서 엑세스 토큰 가져옴
+        const accessToken = localStorage.getItem('access');
+
+        const response = await fetch(`${proxy}/articles/`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) {
+            throw new Error('게시글 작성에 실패하였습니다.');
+        }
+
+        const data = await response.json();
+        console.log(data.message);
+        // 회원가입 완료 후 메인 페이지로 이동
+        window.location.href = "main.html"
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+const createButton = document.getElementById("createButton");
+createButton.addEventListener('click', handleCreateArticle);
--- assets/js/article_detail.js
+++ assets/js/article_detail.js
@@ -1,4 +1,4 @@
-const proxy = 'http://127.0.0.1:8000';  // 게시글 API 엔드포인트
+const proxy = 'https://api.bechol.com';  // 게시글 API 엔드포인트
 
 // 글 상세보기
 
@@ -7,7 +7,6 @@ window.onload = () => {
     ArticleDetail(urlParams);
     loadComments(urlParams);
     CountHeart();
-    CountBookmark();
 }
 
 const article_id = new URLSearchParams(window.location.search).get('id');
